8549ec8454cad92c354e0b803c6d42f7d086f9cb
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-02-19 20:19:12 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 for _ in range(n):
-    line = input()
+    str = input()
     stack = []
-
-    for i in line:
-        if i == '(':
-            stack.append('(')
-        elif i == ')':
+    for j in str:
+        if j == '(':
+            stack.append(j)
+        elif j == ')':
             if len(stack) == 0:
-                stack.append(')')
+                stack.append(j)
                 break
             else:
                 stack.pop()
-
-    if len(stack) != 0:
-        print('NO')
-    else:
+    if len(stack) == 0:
         print('YES')
+    else:
+        print('NO')
