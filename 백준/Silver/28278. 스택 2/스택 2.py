e590d7dcbf315ff9b1de3072ad5e689d0c6c62da
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-02-25 15:55:30 +0900

    [Silver IV] Title: 스택 2, Time: 788 ms, Memory: 88788 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    a = input().split()
+    if a[0] == '1':
+        stack.append(a[1])
+    elif a[0] == '2':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif a[0] == '3':
+        print(len(stack))
+    elif a[0] == '4':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif a[0] == '5':
+       if len(stack) == 0:
+        print(-1)
+       else :
+        print(stack[-1])
+    
