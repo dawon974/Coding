68e91a8666f5943b593e9bb3341d1e9a83124db4
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-02-26 00:19:53 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+wait = list(map(int, input().split()))
+stack = []
+now_turn = 1
+
+for i in wait :
+    stack.append(i)
+    
+    while stack and stack[-1] == now_turn :
+        stack.pop()
+        now_turn+=1
+        
+if stack:
+    print('Sad')
+else:
+    print('Nice')
