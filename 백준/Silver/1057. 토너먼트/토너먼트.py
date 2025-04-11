e64fb87a3259193aaa35e415a3168a8ee4fc0d03
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-04-11 18:29:12 +0900

    [Silver IV] Title: 토너먼트, Time: 40 ms, Memory: 34668 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.py
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -3,11 +3,9 @@ import math
 n, k, l = map(int, input().split())
 cnt = 0
 
-while True:
-    if math.ceil(k) == math.ceil(l):
-        break
-    else:
-        k = math.ceil(k/2)
-        l = math.ceil(l/2)
-        cnt+=1
+while k!=l:
+    k -= k // 2
+    l -= l // 2
+    cnt+=1
+    
 print(cnt)
