ae2dcb198253093e24b7904a376ae9a4829321be
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-04-15 15:56:15 +0900

    [Bronze III] Title: 생장점, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1703. 생장점/생장점.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while 1:
+    line = list(map(int, input().split()))
+    if line[0] == 0:
+        break
+    
+    leaf = 1
+    for i in range(1, len(line), 2):
+        leaf *= line[i]
+        leaf -= line[i + 1]
+    
+    print(leaf)
