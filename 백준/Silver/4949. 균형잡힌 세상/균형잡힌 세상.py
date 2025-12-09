00828e363790bda85621614cc13a0fdb52e211fa
DaWonChoi <80016877+dawon974@users.noreply.github.com>
2025-12-09 16:45:23 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,26 @@
+while True:
+    str = input()
+    
+    if str == '.':
+        break
+    stack = []
+    
+    for i in str:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
+            
